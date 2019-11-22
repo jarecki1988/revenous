a3651d14fd0fc820937b2825bc1f0209713ccdc1
jaroslaw <jarecki1988@gmail.com>
2019-11-22 23:27:57 +0100

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
 import BusinessList from './components/BusinessList/BusinessList';
 
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
 
 class App extends React.Component {
   render(){
@@ -15,6 +26,7 @@ class App extends React.Component {
   )
  }
 }
+
 
 
 export default App;

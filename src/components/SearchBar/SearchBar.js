59bb9eadb37d8b06577ad4b58a82d398c6179ed7
jaroslaw <jarecki1988@gmail.com>
2019-11-25 22:16:06 +0100

    add handleSortByChange to SearchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -23,10 +23,13 @@ class SearchBar extends React.Component {
     }
     return '';
   }
-		renderSortByOptions(){
+  handleSortByChange(sortByOption) {
+    this.setState({sortBy: sortByOption})
+  }
+	renderSortByOptions(){
 			return Object.keys(sortByOptions).map(sortByOption => {
 				let sortByOptionValue = sortByOptions[sortByOption]
-				return <li key={sortByOptionValue} > {sortByOption} </li>
+				return <li key={sortByOptionValue} className={this.getSortByClass(sortByOptionValue)}> {sortByOption} </li>
         } )
 
 		}

aa70f7cfd8c08e904d2e8a32dee5a318b8b736a8
jaroslaw <jarecki1988@gmail.com>
2019-11-26 20:34:03 +0100

    add fetch() with yelp API

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,1 +1,7 @@
-const api = "CUxSlJ4PdrMPBwjlcc9P885gAgWS5krjpwudW4QAYxflu9loLRKN1V3KxTwJx9Igv-TnbYSgs27RRZwoV1UtHZX43QpTtGMKesfHj_jhSMt_KBoa73yqun1GZ2PdXXYx";
+const apiKey = "CUxSlJ4PdrMPBwjlcc9P885gAgWS5krjpwudW4QAYxflu9loLRKN1V3KxTwJx9Igv-TnbYSgs27RRZwoV1UtHZX43QpTtGMKesfHj_jhSMt_KBoa73yqun1GZ2PdXXYx";
+
+const Yelp = {
+    searchYelp(term,location,sortBy) {
+        return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, )
+    }
+}
